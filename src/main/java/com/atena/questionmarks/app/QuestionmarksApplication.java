b58fc8b5e4324974055e924fd1a02d94e274cec3
Jean Marcel de Siqueira <jean.siqueira@outlook.com.br>
2018-12-01 09:20:33 -0200

    first commit

---
+++ src/main/java/com/atena/questionmarks/app/QuestionmarksApplication.java
@@ -0,0 +1,12 @@
+package com.atena.questionmarks.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionmarksApplication.class, args);
+	}
+}
